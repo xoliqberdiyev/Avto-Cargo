3c8f5051dd0e9a34fa00a6fd7c77b11256269775
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-08-29 11:49:19 +0500

    add: add new field to order list serializer

--- core/apps/orders/serializers.py
+++ core/apps/orders/serializers.py
@@ -7,6 +7,6 @@ class OrderListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = [
-            'id', 'name', 'date', 'status', 'size', 'total_price', 'is_paid', 'location', 'location_to',
+            'id', 'order_number', 'name', 'date', 'status', 'size', 'total_price', 'is_paid', 'location', 'location_to',
             'location_from'
         ]
